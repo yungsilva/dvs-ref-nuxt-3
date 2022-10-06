c8cec6fe5a4d59588ae71f4c6be4bbd38c1989c4
Yung Silva <yung@cataline.io>
2022-10-06 19:16:38 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <pre></pre>
+    <pre></pre>
+    <pre></pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, createApp } from 'vue'
+import Counter from './components/Counter.vue'
+
+onMounted(() => {
+  document.querySelectorAll('pre').forEach((preElement) => {
+    const wrapper = document.createElement('div')
+
+    const componentApp = createApp(Counter)
+
+    componentApp.mount(wrapper)
+
+    preElement.append(wrapper)
+  })
+})
+</script>
---
+++ components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="counter">
+    <p>counter: {{ count }}</p>
+    <button @click="count++">increment</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = useState('count', () => 0)
+
+// works fine 👇
+// const count = ref(0)
+</script>
+
+<style scoped>
+.counter {
+  border: 1px solid #ccc;
+  width: max-content;
+  padding: 1rem;
+}
+</style>
